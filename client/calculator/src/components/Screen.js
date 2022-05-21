ef60bb88d1693078c4859c09f2063f46fa87187c
hydroSs1 <michelepierro9548@gmail.com>
2022-05-21 13:12:20 +0200

    add calculator functions

--- client/calculator/src/components/Screen.js
+++ client/calculator/src/components/Screen.js
@@ -1,8 +1,8 @@
 import '../App.css';
 import {Textfit} from "react-textfit";
 
-function CalculatorScreen(value){
-    return(<Textfit classname = "screen" mode = "single" max = {70}>{value}</Textfit>);
+function CalculatorScreen(props){
+    return(<Textfit className = "screen" mode = "single" max = {70}>{props.value}</Textfit>);
 }
 
 export  default CalculatorScreen;
